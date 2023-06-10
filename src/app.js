8f37ed5dc0502af200bd13c155ce31cd64b6546c
Felipe Arce <arceprogramando@gmail.com>
2023-06-09 22:05:18 -0300

    Subida final practica Multer + Router fazt

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import path from 'path'
 import { __dirname } from './utils.js';
-import multer from 'multer';
-import ejs from 'ejs'
+import appRouter from './routes/index.routes.js'
+
 
 // Initializations
 const app = express();
@@ -12,25 +12,12 @@ app.set('port', 3000)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-// middlewares
-app.use(multer({
-    dest: path.join(__dirname, 'public/upload')
-}).single('image'))
-
 //Routes
-app.get('/', (req, res) => {
-    res.render('index');
-})
 
-app.post('/upload', (req, res) => {
-    if (!req.file) {
-        res.status(400).send('No file uploaded.');
-        return;
-    }
+app.use('/', appRouter)
 
-    console.log(req.file);
-    res.send('Uploader');
-});
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(app.get('port'), () => {
     console.log(`Server on post: \n http://localhost:${app.get('port')}`)
